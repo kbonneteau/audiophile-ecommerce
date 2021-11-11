ab2bee60fa37d7923f80b897dc279727a0ec15db
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-11-11 13:42:54 -0800

    Create ProductCategory component. Add initial HTML structure for component.

--- client/src/components/ProductCategoryList/ProductCategoryList.jsx
+++ client/src/components/ProductCategoryList/ProductCategoryList.jsx
@@ -1,11 +1,31 @@
 import './ProductCategoryList.scss';
 import React from 'react';
+import ProductCategory from '../ProductCategory/ProductCategory';
 
 const ProductCategoryList = () => {
+    const productCategories = [
+        {
+            category: "headphones",
+            image: "http://localhost:8080/images/products/categories/image-category-thumbnail-headphones.png"
+        },
+        {
+            category: "speakers",
+            image: "http://localhost:8080/images/products/categories/image-category-thumbnail-speakers.png"
+        },
+        {
+            category: "earphones",
+            image: "http://localhost:8080/images/products/categories/image-category-thumbnail-earphones.png"
+        },
+    ]
+
     return (
-        <div>
-            Category List
-        </div>
+        <section className="product-categories">
+            <div className="product-categories__wrapper">
+                {productCategories.map((category, i) => (
+                    <ProductCategory key={i} category={category} />
+                ))}
+            </div>
+        </section>
     );
 };
 
