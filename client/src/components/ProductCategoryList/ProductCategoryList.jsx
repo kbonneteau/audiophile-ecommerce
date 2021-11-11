9c1b3e042b170624e822aac34f2d5b9192aa641c
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-11-11 12:40:49 -0800

    Create CategoryListComponent

---
+++ client/src/components/ProductCategoryList/ProductCategoryList.jsx
@@ -0,0 +1,12 @@
+import './ProductCategoryList.scss';
+import React from 'react';
+
+const ProductCategoryList = () => {
+    return (
+        <div>
+            Category List
+        </div>
+    );
+};
+
+export default ProductCategoryList;
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import './HomePage.scss';
 import Hero from '../../components/Hero/Hero';
+import ProductCategoryList from '../../components/ProductCategoryList/ProductCategoryList';
 
 const HomePage = () => {
     return (
         <main>
             <Hero />
+            <ProductCategoryList />
         </main>
     );
 };
