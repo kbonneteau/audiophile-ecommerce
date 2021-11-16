0cec97f6b6de25c827627bf5b3a66103b8b92795
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-11-15 21:27:02 -0800

    Add tertiary product feature responsive styles.

--- client/src/components/TertiaryProductFeature/TertiaryProductFeature.jsx
+++ client/src/components/TertiaryProductFeature/TertiaryProductFeature.jsx
@@ -1,11 +1,16 @@
 import './TertiaryProductFeature.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TertiaryProductFeature = () => {
     return (
-        <div>
-            Third feature
-        </div>
+        <section className="tertiary-feature">
+            <div className="tertiary-feature__product-image"></div>
+            <div className="tertiary-feature__product-details">
+                <h2 className="tertiary-feature__product-name">YX1 EARPHONES</h2>
+                <Link className="tertiary-feature__product-link" to="/">See Product</Link>
+            </div>
+        </section>
     );
 };
 
