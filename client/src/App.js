410cfb0fbc33e9eb7f6a176533d56cf82e940d62
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-11-17 22:18:30 -0800

    Initialize Footer component with basic HTML layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
