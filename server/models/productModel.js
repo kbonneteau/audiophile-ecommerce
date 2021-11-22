167beda34952512085031983fb2379c2061bc275
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-11-21 22:00:15 -0800

    Update productModel and productController for getSingleProduct & getProductsByCategory.

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -7,10 +7,21 @@ const connectToDatabase = require("../connect-to-database");
  */
 const readAllProducts = async () => {
     const db = await connectToDatabase();
-    const results = await db.collection("products").find({}).toArray();
+    return await db.collection("products").find({}).toArray();
+};
+
+/**
+ * Connects to database to read all products of the specified category.
+ * @param {string} category name of product category
+ * @returns {array} of all products by category
+ */
+const readProductsByCategory = async category => {
+    const db = await connectToDatabase();
+    const results = await db.collection("products").find({ category: category }).toArray();
     return results;
 };
 
 module.exports = {
-    readAllProducts
+    readAllProducts,
+    readProductsByCategory
 };
--- server/routes/products.js
+++ server/routes/products.js
@@ -4,4 +4,10 @@ const productController = require('../controllers/productController');
 router.route('/')
     .get(productController.getAllProducts);
 
+router.route('/:productId')
+    .get(productController.getSingleProduct);
+
+router.route("/category/:categoryName")
+    .get(productController.getProductByCategory);
+
 module.exports = router;
