81e635f237c446027f66df17caa9cca7432136cc
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-01-01 14:35:58 -0800

    Update Header & Footer icon hover filter. Update HomePage featured products links.

--- client/src/components/SecondaryProductFeature/SecondaryProductFeature.jsx
+++ client/src/components/SecondaryProductFeature/SecondaryProductFeature.jsx
@@ -1,16 +1,21 @@
-import './SecondaryProductFeature.scss';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import "./SecondaryProductFeature.scss";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SecondaryProductFeature = () => {
-    return (
-        <article className="secondary-feature">
-            <div className="secondary-feature__content-container">
-                <h2 className="secondary-feature__product-name">ZX7 SPEAKER</h2>
-                <Link className="secondary-feature__product-link" to="/">See Product</Link>
-            </div>
-        </article>
-    );
+  return (
+    <article className="secondary-feature">
+      <div className="secondary-feature__content-container">
+        <h2 className="secondary-feature__product-name">ZX7 SPEAKER</h2>
+        <Link
+          className="secondary-feature__product-link"
+          to="/speakers/zx7-speaker"
+        >
+          See Product
+        </Link>
+      </div>
+    </article>
+  );
 };
 
 export default SecondaryProductFeature;
